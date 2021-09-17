29973e661904880aa1eb0b27da766a146dc22432
cappellettitadeo <cappellettitadeo@gmail.com>
2021-09-17 20:11:22 +0400

    line chart component, zoom buttons, some cleanups

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <v-app v-bind:style="{ background: $vuetify.theme.themes.light.background }">
-    <Header :tabs="tabs" :appName="'Vue App Template'" />
+    <Header :tabs="[]" :appName="'Vue App Template'" />
     <v-main>
-      <router-view></router-view>
+      <!-- bottom padding 48px because of fixed footer -->
+      <v-container class="pl-6 pr-6 pt-6 pb-12">
+        <router-view></router-view>
+      </v-container>
     </v-main>
     <Footer />
   </v-app>
---
+++ src/views/ML_CAGR_Return.vue
@@ -0,0 +1,64 @@
+<template>
+  <LineChart :series="lineSeries" :fadedLines="faded"/>
+</template>
+
+<script>
+import data1 from "@/modelVersionData/290_16.json";
+import data2 from "@/modelVersionData/290_20.json";
+import data3 from "@/modelVersionData/291_01.json";
+import data4 from "@/modelVersionData/291_03.json";
+import LineChart from "@/components/LineChart";
+
+const transform = (arr) => {
+  return arr.map(d => {
+    return {
+      date: d.effectiveDate,
+      value: d.spreadReturn,
+    };
+  }).sort((a, b) => {
+    if (a.date > b.date) {
+      return 1;
+    } else if (a.date < b.date) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
+
+export default {
+  components: {
+    LineChart
+  },
+  data() {
+    return {
+      faded: [
+        data1.data.getModelMetrics.modelVersionName,
+        data4.data.getModelMetrics.modelVersionName,
+      ],
+      lineSeries: [
+        { 
+          data: transform(data1.data.getModelMetrics.cagrReturns),
+          name: data1.data.getModelMetrics.modelVersionName,
+          color: "#91B36D",
+        },
+        { 
+          data: transform(data2.data.getModelMetrics.cagrReturns),
+          name: data2.data.getModelMetrics.modelVersionName,
+          color: "#D16B5C",
+        },
+        { 
+          data: transform(data3.data.getModelMetrics.cagrReturns),
+          name: data3.data.getModelMetrics.modelVersionName,
+          color: "#EBBE53",
+        },
+        { 
+          data: transform(data4.data.getModelMetrics.cagrReturns),
+          name: data4.data.getModelMetrics.modelVersionName,
+          color: "#87C3D3"
+        }
+      ]
+    }
+  },
+}
+</script>
