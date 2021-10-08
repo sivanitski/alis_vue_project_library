342696d098c5a489fd90616005a7d851861b4e2f
davidspanjaard <dmspanjaard@gmail.com>
2021-10-08 10:04:47 +0200

    feat: add eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    'plugin:vue/recommended',
+    "plugin:vue/strongly-recommended",
+  ],
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": ["warn", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+    "vue/no-unused-vars": "warn",
+    semi: "warn",
+    "vue/max-attributes-per-line": ["warn", {
+      "singleline": 1,
+      "multiline": 1,
+      "allowFirstLine": false,
+    }]
+  },
+};
