f784db7df0044f9d71740abc1ad0be6b32e73b05
davidspanjaard <dmspanjaard@gmail.com>
2021-10-08 08:50:48 +0200

    style: test hook commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,35 @@
 <template>
   <v-row class="text-center">
     <v-col class="mb-4">
-      <h1 class="display-2 font-weight-bold mb-3">
-        We use Vue and Vuetify
-      </h1>
+      <h1 class="display-2 font-weight-bold mb-3">We use Vue and Vuetify</h1>
 
       <p class="subheading font-weight-regular">
         For help and collaboration with other Vuetify developers,
-        <br>please join their online
-        <a
-          href="https://community.vuetifyjs.com"
-          target="_blank"
-        >Discord Community</a>
+        <br />please join their online
+        <a href="https://community.vuetifyjs.com" target="_blank"
+          >Discord Community</a
+        >
       </p>
     </v-col>
-    <v-col
-        class="mb-5"
-        cols="12"
-    >
+    <v-col class="mb-5" cols="12">
       <h2 class="headline font-weight-bold mb-3">
         We use Material Design which is why we use Vuetify
       </h2>
 
       <v-row justify="center">
         <a
-            v-for="(eco, i) in materialDesign"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
+          v-for="(eco, i) in materialDesign"
+          :key="i"
+          :href="eco.href"
+          class="subheading mx-3"
+          target="_blank"
         >
           {{ eco.text }}
         </a>
       </v-row>
     </v-col>
-    <v-col
-      class="mb-5"
-      cols="12"
-    >
-      <h2 class="headline font-weight-bold mb-3">
-        Learn more about Vuetify?
-      </h2>
+    <v-col class="mb-5" cols="12">
+      <h2 class="headline font-weight-bold mb-3">Learn more about Vuetify?</h2>
 
       <v-row justify="center">
         <a
@@ -55,13 +44,8 @@
       </v-row>
     </v-col>
 
-    <v-col
-      class="mb-5"
-      cols="12"
-    >
-      <h2 class="headline font-weight-bold mb-3">
-        Other Useful Vuetify Links
-      </h2>
+    <v-col class="mb-5" cols="12">
+      <h2 class="headline font-weight-bold mb-3">Other Useful Vuetify Links</h2>
 
       <v-row justify="center">
         <a
@@ -79,56 +63,56 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      materialDesign: [
-        {
-          text: 'Material Design',
-          href: 'https://material.io/design/introduction',
-        },
-      ],
-      usefulLinks: [
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Medium Articles',
-          href: 'https://medium.com/vuetify',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      exploreVuetify: [
-        {
-          text: 'Vuetify Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Getting started',
-          href: 'https://vuetifyjs.com/en/getting-started/wireframes/',
-        },
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/cards/',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+  data: () => ({
+    materialDesign: [
+      {
+        text: "Material Design",
+        href: "https://material.io/design/introduction",
+      },
+    ],
+    usefulLinks: [
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Medium Articles",
+        href: "https://medium.com/vuetify",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    exploreVuetify: [
+      {
+        text: "Vuetify Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Getting started",
+        href: "https://vuetifyjs.com/en/getting-started/wireframes/",
+      },
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/cards/",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
 </script>
