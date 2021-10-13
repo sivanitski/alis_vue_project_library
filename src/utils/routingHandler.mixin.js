a015c1eae3362988798242f20999549fbe2b70a2
cappellettitadeo <cappellettitadeo@gmail.com>
2021-10-13 15:58:59 +0200

    feat(alis.resources.view.component): add routing handler

---
+++ src/utils/routingHandler.mixin.js
@@ -0,0 +1,34 @@
+export default {
+    name: "routingHandler",
+
+    methods: {
+        //routeToView pushes to the routeName.
+        //It keeps the state query by default,
+        //currently productTeam as the team={team}
+        //key-value pair. This may be removed by specifying
+        //keepStateQuery = false.
+        //
+        //Takes an optional updatedQuery field which pushes
+        //a query object of key value pairs to the route.
+        // Eg. {team: "organisations/alis/products/in"}
+        //If keepStateQuery = true, the updatedQuery is amended
+        //to the existing state driven query.
+        //If keepStateQuery = false, the updatedQuery replaces
+        //the existing state driven query.
+        routeToView(routeName, params = null, keepStateQuery = true, updatedQuery = null,) {
+
+            if (this.$router.currentRoute.name === routeName) {
+                return;
+            }
+
+            let pushArgs = {
+                name: routeName
+            };
+            if (params) {
+                pushArgs.params = params;
+            }
+
+            this.$router.push(pushArgs);
+        },
+    }
+};
