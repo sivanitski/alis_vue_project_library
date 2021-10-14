e70f4372ea0d1b7e7fdb678131016427efe23f03
davidspanjaard <dmspanjaard@gmail.com>
2021-10-14 10:39:09 +0200

    feat(alis.resources.view.component): add routing format

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import ReturnComparison from "@/library/AAA/ReturnComparison/ReturnComparison";
+import Library from "../views/Library";
+import ReturnComparison from "../library/AAA/ReturnComparison/ReturnComparison";
+import HomeUsefulLinks from "../views/HomeUsefulLinks";
 
 
 Vue.use(VueRouter);
@@ -11,12 +13,34 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    redirect: {name: "Library"},
+    children: [
+      {
+        path: "/resources",
+        name: "Resources",
+        component: HomeUsefulLinks,
+      },
+      {
+        path: "/library",
+        name: "Library",
+        component: Library,
+      },
+
+        // Example
+        // Add a project routes as a child of Home component.
+      {
+        // format path: library/project/{{projectID}}/{{projectContainerName}}
+        path: "library/project/AAA/ReturnComparison",
+
+        // format name: {{projectID}}
+        name: "AAA",
+
+        // format component: {{Semantic name given to component}}.
+        // Component must match the name
+        component: ReturnComparison,
+      }
+    ]
   },
-  {
-    path: "/ML_CAGR_Return",
-    name: "AAA",
-    component: ReturnComparison,
-  }
 ];
 
 const router = new VueRouter({
