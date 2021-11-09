487153db4b3bbca246fb892d28e29aac6bc7f65d
cappellettitadeo <cappellettitadeo@gmail.com>
2021-11-09 13:46:37 +0200

    feat(main): move pop-up container to App.vue

--- src/views/LibraryContainer.vue
+++ src/views/LibraryContainer.vue
@@ -1,44 +1,14 @@
 <template>
   <v-container fluid>
     <router-view />
-    <popup-container
-      v-for="popup in popups"
-      :key="popup.projectID"
-      :config="popup"
-      :title="popup.title || ''"
-      :sub-title="popup.subTitle || ''"
-    >
-      <popup-provider
-        :config="popup"
-      />
-    </popup-container>
   </v-container>
 </template>
 
 <script>
 
-import {mapState} from "vuex";
-import PopupContainer from "@/components/Popups/PopupContainer";
-import PopupProvider from "@/components/Popups/PopupProvider";
 
 export default {
   name: "LibraryContainer",
-  components: {
-    PopupProvider,
-    PopupContainer,
-  },
-  computed: {
-    ...mapState("global", [
-      "popups"
-    ])
-  },
-  watch: {
-    popups: {
-      deep: true,
-      handler: function () {
-      }
-    }
-  }
 };
 </script>
 
